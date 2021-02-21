9c4857198a41b943421ceb70c72a596c0ad21e60
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-02-21 17:16:49 +0000

    Added docstrings, intrinsic matrix and project center functions

--- fat_to_ycb_meta.py
+++ fat_to_ycb_meta.py
@@ -5,24 +5,73 @@ import os
 
 ROOT_PATH = '/Volumes/nik_ext/diss/fat_dataset/fat'
 
-def getCenters(data: dict) -> np.ndarray:
+def get_centers(data: dict) -> np.ndarray:
+  '''
+  Returns n * 2 numpy array of x,y coords of projected centers of objects in image.
+  
+    Parameters:
+      data (dict): JSON data file from FAT dataset
+    
+    Returns:
+      centers (numpy.ndarray): n * 2 numpy array
+  '''
   n = len(data['objects'])
   centers = np.empty((n, 2), dtype=np.float32)
 
   for i, obj in enumerate(data['objects']):
-    print(np.array(obj['projected_cuboid_centroid'], dtype=np.float32))
     centers[i] = obj['projected_cuboid_centroid']
 
   return centers
+
+def get_factor_depth() -> np.ndarray:
+  '''
+  Returns the factor depth parameter.
+
+    Returns:
+      factor_depth (numpy.ndarray): 0-D array containing factor depth
+  '''
+  return np.array(10000, dtype=np.float64)
+
+def get_intrinsic_matrix(camera_data: dict) -> np.ndarray:
+  '''
+  Gets numpy array of the intrinsic matrix from given JSON.
+
+    Parameters:
+      camera_data (dict): Camera JSON file from FAT dataset
+
+    Returns:
+      i_matrix (numpy.ndarray): Intrinsic matrix in numpy format
+  '''
+  i_matrix = np.empty((3, 3), dtype=np.float64)
+
+  i_data = camera_data['camera_settings'][0]['intrinsic_settings']
+  
+  i_matrix[0][0] = '{:e}'.format(i_data['fx'])
+  i_matrix[1][1] = '{:e}'.format(i_data['fy'])
+  i_matrix[2][2] = '{:e}'.format(1)
+  i_matrix[0][1] = '{:e}'.format(i_data['s'])
+  i_matrix[0][2] = '{:e}'.format(i_data['cx'])
+  i_matrix[1][2] = '{:e}'.format(i_data['cy'])
+  i_matrix[1][0] = '{:e}'.format(0)
+  i_matrix[2][0] = '{:e}'.format(0)
+  i_matrix[2][1] = '{:e}'.format(0)
+
+  # TODO: Sort out the precision of the intrinsic matrix to be higher
+  return i_matrix
 
 if __name__ == '__main__':
   print('Creating .mat files...')
 
   with open(ROOT_PATH + '/mixed/kitchen_0/000000.right.json') as f:
     data = json.load(f)
+  
+  with open(ROOT_PATH + '/mixed/kitchen_0/_camera_settings.json') as f:
+    camera_data = json.load(f)
 
   sio.savemat('test.mat', {
-    'center': getCenters(data)
+    'center': get_centers(data),
+    'factor_depth': get_factor_depth(),
+    'intrinsic_matrix': get_intrinsic_matrix(camera_data)
   })
 
   print(sio.whosmat('test.mat'))
