484ca1f39870d04ec5932b5acc9c3e23d7d446ab
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-02-26 15:45:58 +0000

    Initial image conversion script

---
+++ img_conv.py
@@ -0,0 +1,16 @@
+import cv2
+
+ROOT_PATH = '/media/external/diss/fat_dataset/fat'
+
+def crop_image(img):
+  cropped = img[30:30+480][160:160+640]
+
+  return cropped
+
+if __name__ == '__main__':
+  f = ROOT_PATH + '/kitchen_0/000000.right.jpg'
+  img = cv2.imread(f)
+
+  cropped = crop_image()
+  
+  cv2.imwrite('test.jpg', cropped)
