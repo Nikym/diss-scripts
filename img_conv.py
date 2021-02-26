df6dfda2dfa9f98f3adb1700c3bae9e915d2a9c3
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-02-26 15:49:45 +0000

    Small bug fix

--- img_conv.py
+++ img_conv.py
@@ -3,7 +3,7 @@ import cv2
 ROOT_PATH = '/media/external/diss/fat_dataset/fat'
 
 def crop_image(img):
-  cropped = img[30:30+480][160:160+640]
+  cropped = img[30:30+480, 160:160+640]
 
   return cropped
 
@@ -11,6 +11,6 @@ if __name__ == '__main__':
   f = ROOT_PATH + '/kitchen_0/000000.right.jpg'
   img = cv2.imread(f)
 
-  cropped = crop_image()
+  cropped = crop_image(img)
   
   cv2.imwrite('test.jpg', cropped)
