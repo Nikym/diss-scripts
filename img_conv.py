9a10836c9ddd827b467ca39d5a94860ca3d25b91
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-02-27 14:46:10 +0000

    Added basic image cropping

--- img_conv.py
+++ img_conv.py
@@ -1,4 +1,6 @@
 import cv2
+import os
+from .fat_to_ycb_meta import get_directories
 
 ROOT_PATH = '/media/external/diss/fat_dataset/fat'
 
@@ -7,10 +9,97 @@ def crop_image(img):
 
   return cropped
 
-if __name__ == '__main__':
-  f = ROOT_PATH + '/mixed/kitchen_0/000000.right.jpg'
-  img = cv2.imread(f)
+def process_scenes(path: str, start_index: int, output_dir: str = 'output') -> int:
+  '''
+  Processes the image files in a specified directory and outputs image files.
 
-  cropped = crop_image(img)
+    Parameters:
+      path (str): The path of the directory to be processed
+      start_index (int): The index at which the output file names should start at
+      output_dir (str): The path to the output directory
+
+    Returns:
+      num_files_processed (int): The number of files that were processed
+  '''
+  # Each scene has 2 angles with 4 data files each, plus 2 camera files not related (hence -2)
+  num_of_files = int((len(os.listdir(path)) - 2) / 8)
+
+  for x in range(0, num_of_files):
+    for angle in ['left', 'right']:
+      index = x
+      if angle is 'right':
+        index += num_of_files
+      
+      file_name = str(x).zfill(6) + '.' + angle + '.jpg'
+      f = os.path.join(path, file_name)
+
+      try:
+        img = cv2.imread(f)
+
+        cropped_img = crop_image(img)
+
+        cv2.imwrite(
+          output_dir + '/' + str(index + start_index).zfill(6) + '-color.png',
+          cropped_img
+        )
+      except Exception:
+        print('Error! File affected ' + f)
+        quit()
+
+      depth_file_name = str(x).zfill(6) + '.' + angle + 'depth.png'
+      f = os.path.join(path, depth_file_name)
+
+      try:
+        img = cv2.imread(f)
+
+        cropped_img = crop_image(img)
+
+        cv2.imwrite(
+          output_dir + '/' + str(index + start_index).zfill(6) + '-depth.png',
+          cropped_img
+        )
+      except Exception:
+        print('Error! File affected ' + f)
+        quit()
+
+      seg_file_name = str(x).zfill(6) + '.' + angle + 'seg.png'
+      f = os.path.join(path, seg_file_name)
+
+      try:
+        img = cv2.imread(f)
+
+        cropped_img = crop_image(img)
+
+        cv2.imwrite(
+          output_dir + '/' + str(index + start_index).zfill(6) + '-label.png',
+          cropped_img
+        )
+      except Exception:
+        print('Error! File affected ' + f)
+        quit()
+
+if __name__ == '__main__':
+  print('Cropping colour and depth images...')
+
+  dir_list = get_directories(ROOT_PATH + '/')
+
+  log_file = open(ROOT_PATH + '/img_processing_log.txt', 'w+')
+
+  total_files = 0
+  total_dir = len(dir_list)
+
+  log_file.write('Total directories: ' + str(total_dir) + '\n')
+
+  for i, directory in enumerate(dir_list):
+    print('Processing ' + directory + ' ... (start @ ' + 
+      str(total_files) + ', dir ' + str(i+1) + '/' + str(total_dir) + ')')
+    log_file.write('[' + str(total_files).zfill(5) + '] ' + directory + '\n')
+    
+    path = ROOT_PATH + '/' + directory
+    total_files += process_scenes(
+      path,
+      total_files,
+      ROOT_PATH + '/output_img'
+    )
   
-  cv2.imwrite('test.jpg', cropped)
+  log_file.close()
