cd82d4b5e4df22abb952206fce2a2ece3354fcb8
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-03-02 14:20:00 +0000

    Changed how errors in files are handled

--- img_conv.py
+++ img_conv.py
@@ -4,6 +4,8 @@ from fat_to_ycb_meta import get_directories
 
 ROOT_PATH = '/media/external/diss/fat_dataset/fat'
 
+error_list = []
+
 def crop_image(img):
   cropped = img[30:30+480, 160:160+640]
 
@@ -24,62 +26,37 @@ def process_scenes(path: str, start_index: int, output_dir: str = 'output') -> i
   # Each scene has 2 angles with 4 data files each, plus 2 camera files not related (hence -2)
   num_of_files = int((len(os.listdir(path)) - 2) / 8)
 
+  err_count = 0
+  index = 0
   for x in range(0, num_of_files):
     for angle in ['left', 'right']:
-      index = x
-      if angle is 'right':
-        index += num_of_files
+      file_path = os.path.join(path, str(x).zfill(6) + '.' + angle)
       
-      file_name = str(x).zfill(6) + '.' + angle + '.jpg'
-      f = os.path.join(path, file_name)
-
       try:
-        img = cv2.imread(f)
+        file_name = file_path + '.jpg'
+        cropped_img = crop_image(cv2.imread(file_name))
 
-        cropped_img = crop_image(img)
+        depth_file_name = file_path + '.depth.png'
+        cropped_depth = crop_image(cv2.imread(depth_file_name))
 
-        cv2.imwrite(
-          output_dir + '/' + str(index + start_index).zfill(6) + '-color.png',
-          cropped_img
-        )
+        seg_file_name = file_path + '.seg.png'
+        cropped_seg = crop_image(cv2.imread(seg_file_name))
+
+        # Output index adjusted to account for any errors
+        adjusted_index = index - err_count
+        new_file_id = output_dir + '/' + str(adjusted_index + start_index).zfill(6)
+
+        cv2.imwrite(new_file_id + '-color.png', cropped_img)
+        cv2.imwrite(new_file_id + '-depth.png', cropped_depth)
+        cv2.imwrite(new_file_id + '-label.png', cropped_seg)
+        index += 1
       except Exception:
-        print('Error! File affected ' + f)
-        quit()
-
-      depth_file_name = str(x).zfill(6) + '.' + angle + '.depth.png'
-      f = os.path.join(path, depth_file_name)
-
-      try:
-        img = cv2.imread(f)
-
-        cropped_img = crop_image(img)
-
-        cv2.imwrite(
-          output_dir + '/' + str(index + start_index).zfill(6) + '-depth.png',
-          cropped_img
-        )
-      except Exception:
-        print('Error! File affected ' + f)
-        quit()
-
-      seg_file_name = str(x).zfill(6) + '.' + angle + '.seg.png'
-      f = os.path.join(path, seg_file_name)
-
-      try:
-        img = cv2.imread(f)
-
-        cropped_img = crop_image(img)
-
-        cv2.imwrite(
-          output_dir + '/' + str(index + start_index).zfill(6) + '-label.png',
-          cropped_img
-        )
-      except Exception:
-        print('Error! File affected ' + f)
-        quit()
+        print('Error! File affected ' + file_path)
+        error_list.append(file_path)
+        err_count += 1
   
   # Multiplied by 2 as each scene has two angles
-  return num_of_files * 2
+  return num_of_files * 2 - err_count
 
 if __name__ == '__main__':
   print('Cropping colour and depth images...')
