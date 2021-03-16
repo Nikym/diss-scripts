6dfce13ee5f8643824e4b5993d04bf81e1bab8be
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-03-16 09:39:37 +0000

    Updated train file generator to fit new directories

--- generate_train_file.py
+++ generate_train_file.py
@@ -1,7 +1,7 @@
 import os
 from collections import defaultdict
 
-ROOT_PATH = '/media/external/diss/fat_dataset/fat/output/combined'
+ROOT_PATH = '/media/external/diss/fat_dataset/fat/output/sorted'
 POSECNN_PATH = '/home/nikita/diss/PoseCNN/data/LOV'
 
 # Retrieve the number of files in each folder
@@ -17,5 +17,5 @@ for directory in dirs:
 # Create new train file with the relative paths of data
 with open(POSECNN_PATH + '/train_new.txt', 'w') as train_file:
   for n in range(len(file_count.keys())):
-    for scene_id in range(file_count[str(n+1).zfill(3)]):
-      train_file.write(directory + '/' + str(scene_id + n*1200).zfill(6) + '\n')
+    for scene_id in range(file_count[str(n).zfill(3)]):
+      train_file.write(directory + '/' + str(scene_id + n*2000).zfill(6) + '\n')
