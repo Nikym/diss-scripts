b367fdaf25cfdaa8791dd6d5a2eeb49faff3d1f0
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-03-16 09:47:01 +0000

    Added creation of training file

--- generate_train_file.py
+++ generate_train_file.py
@@ -14,8 +14,19 @@ for directory in dirs:
     f for f in os.listdir(ROOT_PATH + '/' + directory) if os.path.isfile(os.path.join(ROOT_PATH, directory, f))
   ])
 
+# % of directories to use for test
+TEST_PERCENTAGE = 10
+
+NUM_TEST_DIRS = len(dirs) // 10
+
 # Create new train file with the relative paths of data
 with open(POSECNN_PATH + '/train_new.txt', 'w') as train_file:
-  for n in range(len(file_count.keys())):
+  for n in range(len(dirs) - NUM_TEST_DIRS):
     for scene_id in range(file_count[str(n).zfill(3)]):
       train_file.write(directory + '/' + str(scene_id + n*2000).zfill(6) + '\n')
+
+# Create new test file with relative paths of data
+with open(POSECNN_PATH + '/test_new.txt', 'w') as test_file:
+  for n in range(len(dirs) - NUM_TEST_DIRS, len(dirs)):
+    for scene_id in range(file_count[str(n).zfill(3)]):
+      test_file.write(directory + '/' + str(scene_id + n*2000).zfill(6) + '\n')
