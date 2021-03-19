b62e290f49a6d715bf4b4b673229f9f93d3a2360
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-03-19 10:23:32 +0000

    Added iteration over the mat files

--- mat_id_change.py
+++ mat_id_change.py
@@ -10,18 +10,27 @@ with open(ROOT_PATH + '/output/object_ids.json') as f:
 
 old_ids = {value: key for key, value in old_ids.items()}
 
-print(old_ids)
-
 with open(ROOT_PATH + '/output/new_object_ids.json') as f:
   new_ids = json.load(f)
 
 count = len([1 for x in list(os.scandir(MAT_ROOT)) if x.is_file()])
 
-# for x in range(count):
-#   scene_id = str(x).zfill(6)
-#   mat_path = MAT_ROOT + '/' + scene_id + '-meta.mat'
+for x in range(count):
+  scene_id = str(x).zfill(6)
+  mat_path = MAT_ROOT + '/' + scene_id + '-meta.mat'
 
-#   data = sio.loadmat(mat_path)
+  data = sio.loadmat(mat_path)
+
+  print(data['cls_indexes'], end='')
+  print(' -> ', end='')
   
-#   for i in len(data['cls_indexes']):
+  for i in len(data['cls_indexes']):
+    old_id = data['cls_indexes'][i][0]
+    new_id = new_ids[old_ids[old_id]]
+
+    data['cls_indexes'][i][0] = new_id
+
+  print(data['cls_indexes'])
+
+print('Complete!')
     
