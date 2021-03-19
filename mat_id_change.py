3ff3058b41d5b768107da19c5267bc4fd156098d
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-03-19 10:14:53 +0000

    Created start of mat_id_change script

---
+++ mat_id_change.py
@@ -0,0 +1,27 @@
+import scipy.io as sio
+import json
+import os
+from common_funcs import ROOT_PATH
+
+MAT_ROOT = ROOT_PATH + '/output/mat'
+
+with open(ROOT_PATH + '/output/object_ids.json') as f:
+  old_ids = json.loads(f)
+
+old_ids = {value: key for key, value in old_ids.}
+
+print(old_ids)
+
+with open(ROOT_PATH + '/output/new_object_ids.json') as f:
+  new_ids = json.loads(f)
+
+count = len([1 for x in list(os.scandir(MAT_ROOT)) if x.is_file()])
+
+# for x in range(count):
+#   scene_id = str(x).zfill(6)
+#   mat_path = MAT_ROOT + '/' + scene_id + '-meta.mat'
+
+#   data = sio.loadmat(mat_path)
+  
+#   for i in len(data['cls_indexes']):
+    
