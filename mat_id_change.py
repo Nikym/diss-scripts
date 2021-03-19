5eb1088a2edc2e52e4521bd3d441323bd0447106
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-03-19 10:17:50 +0000

    Fixed json error and swapped keys for items func

--- mat_id_change.py
+++ mat_id_change.py
@@ -6,14 +6,14 @@ from common_funcs import ROOT_PATH
 MAT_ROOT = ROOT_PATH + '/output/mat'
 
 with open(ROOT_PATH + '/output/object_ids.json') as f:
-  old_ids = json.loads(f)
+  old_ids = json.load(f)
 
-old_ids = {value: key for key, value in old_ids.keys()}
+old_ids = {value: key for key, value in old_ids.items()}
 
 print(old_ids)
 
 with open(ROOT_PATH + '/output/new_object_ids.json') as f:
-  new_ids = json.loads(f)
+  new_ids = json.load(f)
 
 count = len([1 for x in list(os.scandir(MAT_ROOT)) if x.is_file()])
 
