9b8d105d8fb2b2a2b1b5145335e1ad3aebca74d0
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-04-22 15:14:58 +0100

    Added start of new FAT scripts

---
+++ FAT_v2/process_images.py
@@ -0,0 +1,43 @@
+import cv2
+import os
+import json
+from shared_resources import ROOT_DIR, OUT_DIR, NUM_SETS, process_mixed, process_single
+
+def crop_image(img):
+    cropped = img[30:30+480, 160:160+640]
+
+    return cropped
+
+def convert_images(scene_path: str, scene_id: str, out_path: str, new_id: str):
+    '''
+    Convert images to the required names and formats.
+
+        Parameters:
+            scene_path (str): Path to the original scene directory
+            scene_id (str): ID of the scene
+            out_path (str): Path to the output directory
+            new_id (str): New synchronised ID of scene
+    '''
+    color_img = cv2.imread(scene_path + scene_id + '.jpg')
+    cv2.imwrite(out_path + new_id + '-color.png', color_img)
+
+    depth_img = cv2.imread(scene_path + scene_id + '.depth.png', -1)
+    cv2.imwrite(out_path + new_id + '-depth.png', depth_img)
+
+    seg_img = cv2.imread(scene_path + scene_id + '.seg.png', cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite(out_path + new_id + '-label.png', seg_img)
+
+def main():
+    print('*** BEGINNING PROCESSING OF IMAGES ***')
+    try:
+        os.mkdir(OUT_DIR[:-1])
+    except Exception:
+        print('(Directory "processed" already created, skipping creation...)')
+    
+    id_track = process_mixed(convert_images)
+    process_single(id_track, convert_images)
+
+    print('*** COMPLETE ***')
+
+if __name__ == '__main__':
+    main()
---
+++ FAT_v2/shared_resources.py
@@ -0,0 +1,92 @@
+import os
+import json
+
+ROOT_DIR = '/media/nikita/Samsung_T5/diss/fat/'
+OUT_DIR = '/media/nikita/Samsung_T5/diss/fat/output/'
+
+DIRS = [
+    'kitchen_0', 'kitchen_1', 'kitchen_2', 'kitchen_3', 'kitchen_4',
+    'kitedemo_0', 'kitedemo_1', 'kitedemo_2', 'kitedemo_3', 'kitedemo_4',
+    'temple_0', 'temple_1', 'temple_2', 'temple_3', 'temple_4'
+]
+
+NUM_SETS = len(DIRS)
+
+def process_mixed(func) -> int:
+    '''
+    Process the directory structure of the mixed sets of scenes.
+    '''
+    MIXED_DIR = ROOT_DIR + 'mixed/'
+    template = '{root}{dir1}/'
+    print('Processing mixed instance directory...')
+
+    id_track = 0
+    for i, _dir in enumerate(DIRS):
+        id_track = i * 2000
+        padded_dir = str(i).zfill(2)
+        ac_dir = _dir
+        print('    - Looking at set ' + ac_dir + ' ')
+        try:
+            os.mkdir(OUT_DIR + padded_dir)
+        except Exception:
+            print('    (Directory "processed/' + padded_dir + '" already created, skipping creation...)')
+
+        scene_dir = template.format(
+            root=MIXED_DIR, dir1=ac_dir
+        )
+        for _id in range(0, 4000):
+            if (_id % 200 == 0):
+                print('        -> ' + str(_id))
+            if _id >= 2000:
+                angle = 'right'
+            else:
+                angle = 'left'
+
+            ac_id = _id % 2000
+            scene_id = str(ac_id).zfill(6) + '.' + angle
+            new_id = str(_id + id_track).zfill(6)
+
+            func(scene_dir, scene_id, OUT_DIR + padded_dir + '/', new_id)
+        id_track += 4000
+        print('        -> done!')
+
+    return id_track
+
+def process_single(id_track: int, func):
+    '''
+    Process the directory structure of the single sets of scenes.
+    '''
+    SINGLE_DIR = ROOT_DIR + 'single/'
+    template = '{root}{object}/{dir2}/'
+    print('Processing single instance directory...')
+
+    with open('/home/nikita/diss/scripts/objects.json') as f:
+        objects = json.load(f)['objects']
+
+    for i, obj in enumerate(objects):
+        obj = obj + '_16k'
+        padded_dir = str(i + NUM_SETS).zfill(2)
+        print('    - Looking at object ' + obj + ' ')
+        try:
+            os.mkdir(OUT_DIR + padded_dir)
+        except Exception:
+            print('    (Directory "processed/' + padded_dir + '" already created, skipping creation...)')
+
+        for _dir in DIRS:
+            scene_dir = template.format(
+                root=SINGLE_DIR, object=obj, dir2=_dir
+            )
+            print('        -> ' + _dir)
+            for _id in range(0, 200):
+                if _id >= 100:
+                    angle = 'right'
+                else:
+                    angle = 'left'
+
+                ac_id = _id % 100
+                scene_id = str(ac_id).zfill(6) + '.' + angle
+                new_id = str(_id + id_track).zfill(6)
+
+                func(scene_dir, scene_id, OUT_DIR + padded_dir + '/', new_id)
+            id_track += 200
+        print('        -> done!')
