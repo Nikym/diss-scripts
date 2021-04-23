1e95c1cf388aefc2ac420d966cf111f38205eac2
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-04-23 18:21:05 +0100

    Added bash script to auto execute generation

--- genereate_train.py
+++ genereate_train.py
@@ -2,7 +2,7 @@ ROOT_PATH = '/media/nikita/Samsung_T5/diss/generated_data/processed/'
 OUT_PATH = '/home/nikita/diss/scripts/outputs'
 
 # 62 normally
-NUM_DIRS = 80
+NUM_DIRS = 50
 TEST_PERCENTAGE = 20
 
 NUM_VAL_DIRS = int((NUM_DIRS / 100) * TEST_PERCENTAGE)
@@ -10,7 +10,7 @@ print(NUM_VAL_DIRS)
 
 print('Creating train.txt ...')
 with open(OUT_PATH + '/train.txt', 'w') as train_file:
-	for n in range(0, NUM_DIRS - NUM_VAL_DIRS):
+	for n in range(NUM_DIRS - NUM_VAL_DIRS, NUM_DIRS):
 		padded_dir = str(n).zfill(2)
 		for i in range(0, 2000):
 			scene_id = str(i + n*2000).zfill(6)
@@ -18,7 +18,7 @@ with open(OUT_PATH + '/train.txt', 'w') as train_file:
 
 print('Creating val.txt ...')
 with open(OUT_PATH + '/val.txt', 'w') as val_file:
-	for n in range(NUM_DIRS - NUM_VAL_DIRS, NUM_DIRS):
+	for n in range(0, NUM_DIRS - NUM_VAL_DIRS):
 		padded_dir = str(n).zfill(2)
 		for i in range(0, 2000):
 			scene_id = str(i + n*2000).zfill(6)
---
+++ ndds/process.sh
@@ -0,0 +1,7 @@
+python3.7 ndds/process_images.py
+python3.7 ndds/process_box.py
+python3.7 ndds/process_mat.py
+
+# generate training files
+python3.7 generate_train.py
+python3.7 filter_multiple.py
