85c6a7762d01865954347ce4323e4e283b2e682f
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-04-23 21:56:44 +0100

    Fixed typo

--- genereate_train.py
+++ genereate_train.py
@@ -10,7 +10,7 @@ print(NUM_VAL_DIRS)
 
 print('Creating train.txt ...')
 with open(OUT_PATH + '/train.txt', 'w') as train_file:
-	for n in range(NUM_DIRS - NUM_VAL_DIRS, NUM_DIRS):
+	for n in range(NUM_VAL_DIRS, NUM_DIRS):
 		padded_dir = str(n).zfill(2)
 		for i in range(0, 2000):
 			scene_id = str(i + n*2000).zfill(6)
@@ -18,7 +18,7 @@ with open(OUT_PATH + '/train.txt', 'w') as train_file:
 
 print('Creating val.txt ...')
 with open(OUT_PATH + '/val.txt', 'w') as val_file:
-	for n in range(0, NUM_DIRS - NUM_VAL_DIRS):
+	for n in range(0, NUM_VAL_DIRS):
 		padded_dir = str(n).zfill(2)
 		for i in range(0, 2000):
 			scene_id = str(i + n*2000).zfill(6)
