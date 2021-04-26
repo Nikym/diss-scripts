09f3a898d5e86fdce126be647f3d0a2a17f5e968
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-04-27 00:03:45 +0100

    Changed generate script to remove kitedemo

--- FAT_v2/generate.py
+++ FAT_v2/generate.py
@@ -24,36 +24,25 @@ ROOT_PATH = '/media/nikita/Samsung_T5/diss/fat/output/'
 OUT_PATH = '/home/nikita/diss/scripts/outputs'
 
 NUM_DIRS = 36
-TEST_PERCENTAGE = 20
-
-# NUM_VAL_DIRS = int((NUM_DIRS / 100) * TEST_PERCENTAGE)
-# print(NUM_VAL_DIRS)
-
-VALIDATE_DIRS = [0,1,5,6,10,11]
 
 print('Creating list.txt ...')
 with open(OUT_PATH + '/list.txt', 'w') as train_file:
 	for n in range(0, NUM_DIRS):
-		# if n in VALIDATE_DIRS:
-		# 	continue
-
 		padded_dir = str(n).zfill(2)
+		# Uncomment if statement below for no-kitedemo
+		if n > 4 or n < 10:
+			continue
 		if n < 15:
 			for i in range(0, 4000):
 				scene_id = str(i + n*4000).zfill(6)
 				train_file.write(padded_dir + '/' + scene_id + '\n')
 		else:
 			for i in range(0, 3000):
+				# Uncomment if statement below for no-kitedemo
+				if i > 999 or i < 2000:
+					continue
 				scene_id = str(i + 14*4000 + (n-15)*3000).zfill(6)
 				train_file.write(padded_dir + '/' + scene_id + '\n')
-
-# print('Creating val.txt ...')
-# with open(OUT_PATH + '/val.txt', 'w') as val_file:
-# 	for n in VALIDATE_DIRS:
-# 		padded_dir = str(n).zfill(2)
-# 		for i in range(0, 4000):
-# 			scene_id = str(i + n*4000).zfill(6)
-# 			val_file.write(padded_dir + '/' + scene_id + '\n')
 
 filter_file('list')
 
