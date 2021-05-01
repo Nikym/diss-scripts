fe07253592f819b0cd5f51ad13e684e7b7d7bdee
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-05-01 21:44:49 +0100

    Removed random $

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ rm /home/nikita/diss/PoseCNN/data/LOV/keyframe.txt
 cp /home/nikita/diss/PoseCNN/output/fat/lov_train/fat_0008\(13\)/val.txt /home/nikita/diss/PoseCNN/data/LOV/keyframe.txt
 split -l 4000 /home/nikita/diss/PoseCNN/data/LOV/keyframe.txt
 rm /home/nikita/diss/PoseCNN/data/LOV/keyframe.txt
-mv $/home/nikita/diss/PoseCNN/data/LOV/xaa /home/nikita/diss/PoseCNN/data/LOV/keyframe.txt
+mv /home/nikita/diss/PoseCNN/data/LOV/xaa /home/nikita/diss/PoseCNN/data/LOV/keyframe.txt
 rm -r /home/nikita/diss/PoseCNN/data/cache
 cd /home/nikita/diss/PoseCNN
 ./experiments/scripts/fat_data_test_fat.sh 0
